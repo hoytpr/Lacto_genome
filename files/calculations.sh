f36aed947ea0841f1c204779908a83aeb6a9a1fe
Pete <hoytpr@users.noreply.github.com>
2021-10-26 17:15:08 -0500

    Edit

--- files/calculations.sh
+++ files/calculations.sh
@@ -15,8 +15,8 @@ tbp=$((genome*coverage))
 echo "We need at least ${tbp} for the requested coverage"
 gffq1=G-trimmed-Cleaned-Re-paired-Reads1.fastq
 gffq2=G-trimmed-Cleaned-Re-paired-Reads2.fastq
-sed -n '1~4s/^@/>/p;2~4p' ${gffq1} > gffa1  # G-trimmed fasta file R1
-sed -n '1~4s/^@/>/p;2~4p' ${gffq2} > gffa2  # G-trimmed fasta file R2
+sed -n '1~4s/^@/>/p;2~4p' ${gffq1} > gffa1  
+sed -n '1~4s/^@/>/p;2~4p' ${gffq2} > gffa2 
 r1=`wc -l < gffa1`
 r2=`wc -l < gffa2`
 rd1=$((r1/n2))
