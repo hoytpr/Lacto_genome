f8ba4f0b5ecbd6ec6e40668a63494eb571448ddc
Pete <hoytpr@users.noreply.github.com>
2022-09-23 13:23:11 -0500

    Typo removed

--- files/lactoscript-only.sh
+++ files/lactoscript-only.sh
@@ -22,7 +22,7 @@
 `java -jar /opt/trimmomatic/0.38/prebuilt/trimmomatic-0.38.jar PE -threads 4 L_lactis_S1_LALL_R1.trim.fastq.gz L_lactis_S1_LALL_R2.trim.fastq.gz \
             L_lactis_S1_LALL_R1.trim.Gtrim.fastq.gz L_lactis_S1_LALL_R1.trim.un.Gtrim.fastq.gz \
             L_lactis_S1_LALL_R2.trim.Gtrim.fastq.gz L_lactis_S1_LALL_R2.trim.un.Gtrim.fastq.gz \
-            SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE-G.fa:2:40:15`
+            SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15`
 
 #### BBTools re-pair script:
 
